ab66bcd21ba45425f8fb9e90f0aa847390148a7c
Ajaykumarannn <ajaykumaran07@gmail.com>
2024-08-08 13:03:53 +0530

    PlayerMovement
    Inputactionbasics

--- Assets/Scripts/Learning/LearningDemo.cs
+++ Assets/Scripts/Learning/LearningDemo.cs
@@ -1,19 +1,38 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LearningDemo : MonoBehaviour
+
 {
+    bool testBoolean = false;
+    int testInteger = -3;
+    float testFloat = 0.5f;
+    string testString = "Oomb";
+
     // Start is called before the first frame update
     void Start()
     {
+        float x = FindSinValue(testFloat);
         
+        Debug.Log(x);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    float FindSinValue(float _incomingAngle)
+    { 
+        float result;
+
+        result = Mathf.Sin(_incomingAngle);
+        
+        return result;
+    
     }
 }
